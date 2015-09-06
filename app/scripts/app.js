9e07ff5756a7a0c2619ced9ffd73c81a2b47651e
RoyMontoya <roy_a18@hotmail.com>
2015-09-06 15:12:45 -0700

    fixed persistance bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,29 +9,33 @@
    *
    * Main module of the application.
    */
-  angular
-    .module('mytodoApp', [
-      'ngAnimate',
-      'ngCookies',
-      'ngResource',
-      'ngRoute',
-      'ngSanitize',
-      'ngTouch',
-      'ui.sortable'
-    ])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
-        })
-        .when('/about', {
-          templateUrl: 'views/about.html',
-          controller: 'AboutCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-    });
+   angular
+     .module('mytodoApp', [
+       'ngAnimate',
+       'ngCookies',
+       'ngResource',
+       'ngRoute',
+       'ngSanitize',
+       'ngTouch',
+       'ui.sortable',
+       'LocalStorageModule'
+     ])
+     .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+       localStorageServiceProvider.setPrefix('ls');
+     }])
+     .config(function ($routeProvider) {
+       $routeProvider
+         .when('/', {
+           templateUrl: 'views/main.html',
+           controller: 'MainCtrl'
+         })
+         .when('/about', {
+           templateUrl: 'views/about.html',
+           controller: 'AboutCtrl'
+         })
+         .otherwise({
+           redirectTo: '/'
+         });
+     });
 
 })(angular);
